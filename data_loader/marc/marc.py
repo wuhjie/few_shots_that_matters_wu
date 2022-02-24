ae953d73b799ab701fcb3e17784ce1148bc3949a
wuhjie <wu.hongjie@outlook.com>
2022-02-24 23:23:04 +0000

    only pos

--- data_loader/marc/marc.py
+++ data_loader/marc/marc.py
@@ -43,7 +43,7 @@ class MARCDataset(MultilingualRawDataset):
                     which_split = os.path.join("dev", f"dataset_{lang}_dev.json")
                 elif which_split == "test":
                     which_split = os.path.join("test", f"dataset_{lang}_test.json")
-                file_ = os.path.join(marc_, which_split)
+                file_ = os.path.join(_, which_split)
                 entries.extend(self.marc_parse(lang, file_, wsplit))
         entries = sorted(entries, key=lambda x: x[0])  # groupby requires contiguous
         for language, triplets in itertools.groupby(entries, key=lambda x: x[0]):

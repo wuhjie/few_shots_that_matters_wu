fbf60a0b0abfdd57b9da29fba316c4414d1b4e3a
wuhjie <wu.hongjie@outlook.com>
2022-03-11 01:36:01 +0000

    german, no english

--- run_ckpt.sh
+++ run_ckpt.sh
@@ -7,12 +7,12 @@ declare -a list_of_finetune_lr=(1e-5)
 
 for ((which_finetune_lr=0;which_finetune_lr<${#list_of_finetune_lr[@]};++which_finetune_lr)); do
     python finetuning_baseline.py --override False \
-        --experiment marc_ckpts \
+        --experiment udpos_ckpts \
         --ptl bert \
         --model bert-base-multilingual-cased \
         --dataset_name $TASK \
-        --trn_languages english \
-        --eval_languages german,english,spanish,french,chinese,japanese \
+        --trn_languages german \
+        --eval_languages german,spanish,french,chinese,japanese \
         --finetune_epochs 10 \
         --finetune_batch_size 32 \
         --eval_every_batch 50 \

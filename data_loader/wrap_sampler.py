e3bc7501dd1c2f2842e342132be3e8ddffa1a0db
wuhjie <wu.hongjie@outlook.com>
2022-04-07 22:56:53 +0100

    1

--- data_loader/wrap_sampler.py
+++ data_loader/wrap_sampler.py
@@ -5,13 +5,13 @@ from future.active_learning import al_with_pool
 
 def wrap_sampler(trn_batch_size, infer_batch_size, language, language_dataset):
 
-    # performance_history, predictions = al_with_pool(language_dataset.raw_dataset.trn_egs)
+    performance_history, predictions = al_with_pool(language_dataset.trn_egs)
     
 
-    # print("performance history: ", performance_history)
+    print("performance history: ", performance_history)
     # print("ptrdictions: ", predictions)
 
-    print("trn_dataset: ", language_dataset.trn_egs)
+    # print("trn_dataset: ", language_dataset.trn_egs)
 
     #TODO: select with lowest predictions
 

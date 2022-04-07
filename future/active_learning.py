4101d1097e1e7b2b24ee31e1429b45edd764d2cf
wuhjie <wu.hongjie@outlook.com>
2022-04-07 23:00:16 +0100

    1

--- future/active_learning.py
+++ future/active_learning.py
@@ -32,7 +32,7 @@ def al_with_pool(trn_data):
 
     print("training indices: ", training_indices)
 
-    X_train, tag_train = X[training_indices].reshape(-1, 1), tag[training_indices]
+    X_train, tag_train = X[training_indices], tag[training_indices]
     X_pool, tag_pool = np.delete(X, training_indices, axis=0), np.delete(tag, training_indices, axis=0)
 
     print("x train: ", X_train)

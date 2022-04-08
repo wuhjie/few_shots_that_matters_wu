d2b27e8f94212a4deed6b3b3907ef63a994e734a
wuhjie <wu.hongjie@outlook.com>
2022-04-08 14:45:44 +0100

    test

--- future/active_learning.py
+++ future/active_learning.py
@@ -24,9 +24,8 @@ def extract(lst, n):
 def al_with_pool(trn_data):
     # X_raw, tag_raw = extract(trn_data, 0),  extract(trn_data, 1)
 
-    X_raw, tag_raw = trn_data.uides, trn_data.tags_ides
+    X, tag = trn_data.uides, trn_data.tags_ides
 
-    X, tag = np.array(X_raw), np.array(tag_raw)
     X_length = X.shape[0]
 
 # 80/20 split
@@ -34,8 +33,8 @@ def al_with_pool(trn_data):
 
     print("training indices: ", training_indices)
 
-    X_train, tag_train = X[training_indices].reshape(1, -1), tag[training_indices]
-    X_pool, tag_pool = np.delete(X[0], training_indices, axis=0).reshape(1, -1), np.delete(tag, training_indices, axis=0)
+    X_train, tag_train = X[training_indices], tag[training_indices]
+    X_pool, tag_pool = np.delete(X, training_indices, axis=0), np.delete(tag, training_indices, axis=0)
 
     print("x train shape: ", X_train.shape)
     print("tag train shape: ", tag_train.shape)

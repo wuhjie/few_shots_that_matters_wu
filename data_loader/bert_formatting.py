35936b6930df2e0b143aa299cf8ccc2fc4510308
wuhjie <wu.hongjie@outlook.com>
2022-04-11 13:32:46 +0100

    ...

--- data_loader/bert_formatting.py
+++ data_loader/bert_formatting.py
@@ -169,8 +169,8 @@ def tagging_example_to_feature(which_split, tagged_sents, tokenizer, t2i, msl):
         )
 
         print("length of all_fts: ", len(all_fts))
-        for f in all_fts:
-            print("f input ids: ", f.input_ids)
+        # for f in all_fts:
+        #     print("f input ids: ", f.input_ids)
 
 
     print("[WARN]: {} sentences longer than msl ...".format(len(toolongs)))

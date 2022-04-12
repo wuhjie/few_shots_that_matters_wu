b1e5787f164d577991371efed2972a491be2b26d
wuhjie <wu.hongjie@outlook.com>
2022-04-12 12:53:48 +0100

    k

--- data_loader/wrap_sampler.py
+++ data_loader/wrap_sampler.py
@@ -8,28 +8,33 @@ def wrap_sampler(trn_batch_size, infer_batch_size, language, language_dataset):
     for split_name in ("trn_egs", "val_egs", "tst_egs"):
         egs = getattr(language_dataset, split_name)
 
-        # print("egs: ", egs)
-
         if len(egs) == 0:
             print(f"[WARN] {split_name} of {language} has zero egs")
         if split_name == "trn_egs":  
             # TODO: only select those with highest uncertainty 
             # input_idses, tags_ides 
 
-            all_history, all_predictions = al_pool(egs)
+            all_history, all_predictions, lowest_score_index = al_pool(egs)
             print("performance history", all_history)
-            print("all predictions: ", all_predictions)
+            print("score index: ", lowest_score_index)
+
+            eg_name = egs.uides[lowest_score_index]
+            dl = (
+                DataLoader(egs.uides[lowest_score_index], sampler=sampler(egs.uides[lowest_score_index]), batch_size=batch_size)
+                if len(egs) > 0
+                else None
+            )   
 
             sampler = RandomSampler
             batch_size = trn_batch_size
         else:
             sampler = SequentialSampler
             batch_size = infer_batch_size
-        dl = (
-            DataLoader(egs, sampler=sampler(egs), batch_size=batch_size)
-            if len(egs) > 0
-            else None
-        )    
+            dl = (
+                DataLoader(egs, sampler=sampler(egs), batch_size=batch_size)
+                if len(egs) > 0
+                else None
+            )    
 
         # print("batch size: ", batch_size)
 

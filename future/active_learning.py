5cb11dbc395f5bf0fb0482c1c3eba9481790f28d
wuhjie <wu.hongjie@outlook.com>
2022-04-12 11:12:07 +0100

    eg_item

--- future/active_learning.py
+++ future/active_learning.py
@@ -18,35 +18,20 @@ np.random.seed(RANDOM_STATE_SEED)
 
 N_QUERIES = 10
 
-def extract(lst, n):
-    return np.array(list(list(zip(*lst[0]))[n]))
-
-def indices_data_mapping(egs, indices):
-    mapped_egs = []
-    for i in indices:
-        mapped_egs.append(egs[i])
-    return mapped_egs
-
 def tensor_to_np(egs_item):
-    return np.array([e.tolist() for e in egs_item])
+    # return np.array([e.tolist() for e in egs_item])
+    return np.array(egs_item)
 
 def al_with_pool(egs):
-    
-    # length of each segmentation is 128
-    # X, tag = egs.input_idses[0], egs.tags_ides[0]
 
     performance_history = 0
+
+    # reshape for ActiveLearner
     X, tag = tensor_to_np(egs.input_idses[0]).reshape(-1, 1), tensor_to_np(egs.tags_ides[0])
-
-    # print("X: ", X)
-    # print("tags: ", tag)
-
     X_length = X.shape[0]
 
 # 80/20 split
     training_indices = random.sample(range(0, X_length-1), int(X_length*0.8))
-
-    # print("training indices: ", training_indices)
 
     X_train, tag_train = X[training_indices], tag[training_indices] 
     X_pool, tag_pool = np.delete(X, training_indices, axis=0), np.delete(tag, training_indices, axis=0)

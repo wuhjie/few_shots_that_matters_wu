af61384ff8eb5d29d11feafa005f636fc756039a
wuhjie <wu.hongjie@outlook.com>
2022-04-13 21:06:14 +0100

    123

---
+++ active_learning/uncertainty_sampling.py
@@ -0,0 +1,5 @@
+'''
+pool-based sampling -> scrnario
+uncertainty-based sampling -> query strategy framework
+example url: https://modal-python.readthedocs.io/en/latest/content/examples/pool-based_sampling.html
+'''

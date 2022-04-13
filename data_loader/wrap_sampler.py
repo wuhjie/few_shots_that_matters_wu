eb8cd0f0cfc3ea7e5ea40f412b6b73827034bc2f
wuhjie <wu.hongjie@outlook.com>
2022-04-13 16:41:01 +0100

    divide

--- data_loader/wrap_sampler.py
+++ data_loader/wrap_sampler.py
@@ -1,6 +1,6 @@
 from torch.utils.data import SequentialSampler, DataLoader, RandomSampler
 
-from future.active_learning import al_pool
+from active_learning.query_by_committee import al_pool
 
 
 def wrap_sampler(trn_batch_size, infer_batch_size, language, language_dataset):
@@ -11,25 +11,13 @@ def wrap_sampler(trn_batch_size, infer_batch_size, language, language_dataset):
         if len(egs) == 0:
             print(f"[WARN] {split_name} of {language} has zero egs")
         if split_name == "trn_egs":  
-            # TODO: only select those with highest uncertainty 
             # input_idses, tags_ides 
+            # all_history, all_predictions, lowest_score_index = al_pool(egs)
 
-            all_history, all_predictions, lowest_score_index = al_pool(egs)
-            print("performance history", all_history)
-            print("score index: ", lowest_score_index)
-
-            eg_name = egs.uides[lowest_score_index]
-            print("eg_name: ", eg_name)
-
-            # sampler = RandomSampler
-            # batch_size = trn_batch_size
-
-            # TODO:
-            dl = (
-                DataLoader(egs, sampler=sampler, batch_size=batch_size)
-                if len(egs) > 0
-                else None
-            )   
+            # TODO: replace the random sampler with uncertainty sampling
+            sampler = RandomSampler
+            batch_size = trn_batch_size
+  
 
         else:
             sampler = SequentialSampler

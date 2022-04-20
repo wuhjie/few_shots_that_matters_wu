a481a54a0f192f10290fc2747999aa9852903bc5
wuhjie <wu.hongjie@outlook.com>
2022-04-20 22:57:21 +0100

    logits

--- future/adapt_trainer.py
+++ future/adapt_trainer.py
@@ -77,10 +77,8 @@ class AdaptTuner(BaseTrainer):
                 batched, golds, uids, _golds_tagging = self.collocate_batch_fn(batched)
                 # TODO: 
                 logits, *_ = self._model_forward(self.model, **batched)
+                print("logits: ", logits)
 
-                print("batched: ", batched)
-                print("**batched: ", **batched)
-                
                 loss = self.criterion(logits, golds).mean()
                 epoch_losses.append(loss.item())
                 loss.backward()

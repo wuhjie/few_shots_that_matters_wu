b26301d2a3ee22f385b475e9c065b2f55f882a85
wuhjie <wu.hongjie@outlook.com>
2022-04-21 01:21:49 +0100

    update

--- active_learning/uncertainty_sampling.py
+++ active_learning/uncertainty_sampling.py
@@ -17,46 +17,13 @@ Every Sampler subclass has to provide an __iter__() method,
 providing a way to iterate over indices of dataset elements, 
 and a __len__() method that returns the length of the returned iterators.
 '''
-class Sampler(Generic[T_co]):
-    def __init__(self, data_source: Optional[Sized]) -> None:
-        pass
 
-    def __iter__(self) -> Iterator[T_co]:
-        raise NotImplementedError
-
-class UncertaintySampler(Sampler[int]):
-    data_source: Sized
-    # replacement: bool
-
-    def __init__(self, data_source: Sized,
-                 prob_list=[]) -> None:
-        self.data_source = data_source
-        self.prob_list = prob_list
-
-        # if not isinstance(self.replacement, bool):
-        #     raise TypeError("replacement should be a boolean value, but got "
-        #                     "replacement={}".format(self.replacement))
-        
-    @property
-    def num_samples(self) -> int:
-        if self._num_samples is None:
-            return len(self.data_source)
-        return self._num_samples
-
-    def __len__(self) -> int:
-        return self.num_samples
-    
-    def __iter__(self) -> Iterator[int]:
-        least_conf = least_confidence(self.prob_list)
-        print("least_conf: ", least_conf)
-
-
-def least_confidence(prob_dist):
+def least_confidence(probs, egs):
     # most confidence prediction
-    simple_least_conf = torch.max(torch.as_tensor(prob_dist))
+    simple_least_conf = torch.max(torch.as_tensor(probs))
     
     # number of data, for torch
-    num_labels = prob_dist.numel()
+    num_labels = probs.numel()
 
     normalized_least_conf = (1-simple_least_conf) * (num_labels / (num_labels-1))
 

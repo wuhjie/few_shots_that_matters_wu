932b08e30a13c744f66e2d76cc3cf3f4ffead6ab
wuhjie <wu.hongjie@outlook.com>
2022-04-22 15:57:13 +0100

    update

--- active_learning/uncertainty_sampling.py
+++ active_learning/uncertainty_sampling.py
@@ -7,16 +7,39 @@ example url: https://github.com/rmunro/pytorch_active_learning/blob/master/uncer
 
 import torch
 
-
-
-def least_confidence(probs, egs):
-#     # most confidence prediction
-#     # simple_least_conf = torch.max(torch.as_tensor(probs))
+def softmax(scores, base=math.e):
+        """Returns softmax array for array of scores
+        
+        Converts a set of raw scores from a model (logits) into a 
+        probability distribution via softmax.
+            
+        The probability distribution will be a set of real numbers
+        such that each is in the range 0-1.0 and the sum is 1.0.
     
-#     # number of data, for torch
-#     num_labels = probs.numel()
-#     return _
-    return 0
+        Assumes input is a pytorch tensor: tensor([1.0, 4.0, 2.0, 3.0])
+            
+        Keyword arguments:
+            prediction -- a pytorch tensor of any positive/negative real numbers.
+            base -- the base for the exponential (default e)
+        """
+        exps = (base**scores.to(dtype=torch.float)) # exponential for each value in array
+        sum_exps = torch.sum(exps) # sum of all exponentials
 
-#     return normalized_least_conf.item()
+        prob_dist = exps / sum_exps # normalize exponentials 
+        return prob_dist
+
+def least_confidence(logits):
+    print("logits: ", logits)
+
+    logits_softmax = softmax(logits)
+    print("logits_softmax: ", logits_softmax)
+    print("length of softmax: ", len(logits_softmax))
+
+    sentence_logits = [sum(logit) for logit in logits]
+    print("sentence logits: ", sentence_logits)
+    print("length of sentence logits: ", len(sentence_logits))
+    
+    min_sentence_logits = min(sentence_logits)
+    min_sentence_logits_index = sentence_logits.index(min_sentence_logits)
+    print("the min is: ", min_sentence_logits, 'with index ', min_sentence_logits_index)
 
--- future/adapt_trainer.py
+++ future/adapt_trainer.py
@@ -5,6 +5,8 @@ import numpy as np
 import math
 
 from copy import deepcopy
+
+from active_learning.uncertainty_sampling import least_confidence
 from .base import BaseTrainer
 from .hooks.base_hook import HookContainer
 from .hooks import EvaluationRecorder
@@ -78,19 +80,7 @@ class AdaptTuner(BaseTrainer):
                 # TODO: logits for uncertainty sampling
                 logits, *_ = self._model_forward(self.model, **batched)
                 print("batch: ", batched)
-                print("logits: ", logits)
-
-                logits_softmax = softmax(logits)
-                print("logits_softmax: ", logits_softmax)
-                print("length of softmax: ", len(logits_softmax))
-
-                sentence_logits = [sum(logit) for logit in logits]
-                print("sentence logits: ", sentence_logits)
-                print("length of sentence logits: ", len(sentence_logits))
-                
-                min_sentence_logits = min(sentence_logits)
-                min_sentence_logits_index = sentence_logits.index(min_sentence_logits)
-                print("the min is: ", min_sentence_logits, 'with index ', min_sentence_logits_index)
+                least_confidence(logits)
 
                 loss = self.criterion(logits, golds).mean()
                 epoch_losses.append(loss.item())
@@ -163,24 +153,3 @@ class AdaptTuner(BaseTrainer):
             learning_curves=learning_curves, tst_scores=tst_scores,
         )
         return
-
-def softmax(scores, base=math.e):
-        """Returns softmax array for array of scores
-        
-        Converts a set of raw scores from a model (logits) into a 
-        probability distribution via softmax.
-            
-        The probability distribution will be a set of real numbers
-        such that each is in the range 0-1.0 and the sum is 1.0.
-    
-        Assumes input is a pytorch tensor: tensor([1.0, 4.0, 2.0, 3.0])
-            
-        Keyword arguments:
-            prediction -- a pytorch tensor of any positive/negative real numbers.
-            base -- the base for the exponential (default e)
-        """
-        exps = (base**scores.to(dtype=torch.float)) # exponential for each value in array
-        sum_exps = torch.sum(exps) # sum of all exponentials
-
-        prob_dist = exps / sum_exps # normalize exponentials 
-        return prob_dist

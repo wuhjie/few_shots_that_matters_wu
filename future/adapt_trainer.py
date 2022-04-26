6b8195f39ad5842d2482ff4a9fbf1533ff9d2b8c
wuhjie <wu.hongjie@outlook.com>
2022-04-26 04:03:55 +0100

    update

--- future/adapt_trainer.py
+++ future/adapt_trainer.py
@@ -155,9 +155,9 @@ class AdaptTuner(BaseTrainer):
             #     language_dataset=language_dataset,
             #     )
             
-        uncertainty_id_one_epoch.append(least_confidence(logits))
-        
-        print("id list after training: ", torch.unique(uncertainty_id_one_epoch))
+        uncertainty_id_one_epoch.append(torch.unique(least_confidence(logits)))
+
+        print("id list after training: ", uncertainty_id_one_epoch)
 
         # test
         tst_scores = self._infer_tst_egs(

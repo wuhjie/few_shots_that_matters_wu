de706494af705c081dcc7f50badb6fb99ca69672
wuhjie <wu.hongjie@outlook.com>
2022-05-02 19:41:52 +0100

    update

--- data_loader/task_configs.py
+++ data_loader/task_configs.py
@@ -1,11 +1,4 @@
-# from .glue.datasets import MRPCDataset
-from .mldoc import MLDocDataset
-from .marc import MARCDataset
-# from .ner import CONLL2003Dataset, PANXDataset
-from .ner import PANXDataset
-# from .argus import ARGUStanceDataset
-from .pawsx import PAWSXDataset
-from .NLI import XNLIDataset
+
 from .pos import UDPOSDataset
 
 # from sampled_data_loader.mldoc.mldoc import SampledMLDocDataset

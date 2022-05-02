caaf2dd02a21d15e0ad76482509bafd03f9ad8be
wuhjie <wu.hongjie@outlook.com>
2022-05-02 19:40:36 +0100

    update

--- run_ckpt.sh
+++ run_ckpt.sh
@@ -11,7 +11,7 @@ for ((which_finetune_lr=0;which_finetune_lr<${#list_of_finetune_lr[@]};++which_f
         --model bert-base-multilingual-cased \
         --dataset_name udpos \
         --trn_languages english \
-        --eval_languages german \
+        --eval_languages finnish \
         --finetune_epochs 10 \
         --finetune_batch_size 32 \
         --eval_every_batch 50 \

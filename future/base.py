0e1e41a81ddb78cccaa16b93ac80d048f4d4ac10
wuhjie <wu.hongjie@outlook.com>
2022-05-02 19:50:41 +0100

    update

--- future/base.py
+++ future/base.py
@@ -26,7 +26,7 @@ class BaseTrainer(object):
 
     def _parallel_to_device(self, model):
         model = model.cuda()
-        if len(self.conf.world) > 1:
+        if len(self.conf.world) >= 1:
             model = torch.nn.DataParallel(model, device_ids=self.conf.world)
         return model
 

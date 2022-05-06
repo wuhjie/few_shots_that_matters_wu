dbd92dad87ca0991a97038beebb4791c73d5a019
wuhjie <wu.hongjie@outlook.com>
2022-05-06 03:02:48 +0100

    dutch finnish

--- active_adapt.sh
+++ active_adapt.sh
@@ -6,7 +6,7 @@ declare -a ckpt_path=data/checkpoint_adapt
 declare -a list_of_adapt_trn_languages=(dutch, finnish)
 # (1e-5 3e-5 5e-5 7e-5)
 declare -a list_of_adapt_lr=(1e-5)
-declare -a list_of_num_shots=(1 2)
+declare -a list_of_num_shots=(2)
 # group: bucket; we sampled 40 buckets for each target
 declare -a list_of_group_index=(0 1 2 3 4)
 
